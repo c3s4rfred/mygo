acc9c2d78bb85bf9989c8e1ceb419d3f30e15888
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2023-02-03 10:29:31 -0500

    Initial commit of code

---
+++ SimpleServer.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Go quote", quote.Go())
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/Hi", handlerHi)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This the root\n")
+}
+func handlerHi(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This the Hi\n")
+}
