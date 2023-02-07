563e3c85f330d0c2b88095b96cb647027fd112df
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2023-02-07 16:53:43 -0500

    Using other package, exporting func

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module "github.com/c3s4rfred/mygo"
 
-go 1.17
+go 1.20
---
+++ nextpkg/nextpkg.go
@@ -0,0 +1,12 @@
+package nextpkg
+
+import (
+	"fmt"
+)
+
+func NextP1() {
+	fmt.Printf("This the nextpkg func 1\n")
+}
+func NextP2() {
+	fmt.Printf("This the nextpkg func 2\n")
+}
